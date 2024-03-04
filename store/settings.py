e3f80437ca73acd9131665f68bed96f80245148d
revillnik <revillnik@gmail.com>
2024-03-04 22:12:40 +0300

    next

--- store/settings.py
+++ store/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILRES_DIRS =[BASE_DIR / "static",]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
