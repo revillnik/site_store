8260d7396a4e11c852e4d446d28b3dafcbf55cc4
revillnik <revillnik@gmail.com>
2024-03-04 22:26:14 +0300

    next

--- store/settings.py
+++ store/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "store.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "products" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILRES_DIRS =[BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
