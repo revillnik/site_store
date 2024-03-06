163aca9eb7f8b16e9905c0cd4ebb85eac63cd686
revillnik <revillnik@gmail.com>
2024-03-06 23:59:50 +0300

    next

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    quantity = models.PositiveIntegerField(defailt=0)
+    image = models.ImageField(upload_to="products_images")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT)
+    
