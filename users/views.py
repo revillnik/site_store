8f5928d693c4aab0adb589244d981c9722093822
revillnik <revillnik@gmail.com>
2024-03-08 19:59:53 +0300

    next

--- users/views.py
+++ users/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return redirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {"form": form}
+    return render(request, "users/login.html", context)
 
 
 def register(request):
