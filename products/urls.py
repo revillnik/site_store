ba80c9c7f70cc31e8eb2cb4372d7f868eb8fd8fa
revillnik <revillnik@gmail.com>
2024-03-08 18:58:41 +0300

    next

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
 from products.views import index, products
-from django.conf.urls.static import static
-from django.conf import settings
 
 app_name = "products"
 
